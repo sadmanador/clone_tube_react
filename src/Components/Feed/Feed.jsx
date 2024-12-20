917724750367fd3227ce38d1796d16b8b3876e10
sadmanador <sadman.ador@gmail.com>
2024-12-20 12:16:43 +0600

    Home page is done

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,15 +1,8 @@
 import React, { useEffect, useState } from "react";
-import "./Feed.css";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
 import { Link } from "react-router-dom";
-import { API_KEY } from "../../data";
+import { API_KEY, value_converter } from "../../data";
+import "./Feed.css";
+import moment from "moment";
 
 const Feed = ({ category }) => {
   const [data, setData] = useState([]);
@@ -49,7 +42,7 @@ const Feed = ({ category }) => {
             <img src={item.snippet.thumbnails.medium.url} alt="" />
             <h2>{item.snippet.title}</h2>
             <h3>{item.snippet.channelTitle}</h3>
-            <p>{item.statistics.viewCount} views &bull; 2 days ago</p>
+            <p>{value_converter(item.statistics.viewCount)} views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
           </Link>
         );
       })}
